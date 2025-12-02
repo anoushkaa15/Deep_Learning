3e412e6fbb3a081214aa013db96dd60c7233b49d
anoushkaa15 <161074890+anoushkaa15@users.noreply.github.com>
2025-12-03 03:08:38 +0530

    Remove data loader function from utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,1 @@
-from torch.utils.data import DataLoader
-from .dataset import AgriVision4ChDataset
 
-def get_loaders(root, batch=16):
-    train = AgriVision4ChDataset(root, split='train')
-    val = AgriVision4ChDataset(root, split='val')
-    return (
-        DataLoader(train, batch_size=batch, shuffle=True, num_workers=2, drop_last=True),
-        DataLoader(val, batch_size=batch, shuffle=False, num_workers=2, drop_last=True)
-    )
